987e2b0b65aff04458bae2c53a31d5f11b8d9399
githubmaite <marit.rivas@gmail.com>
2022-04-05 21:58:45 -0400

    version 5 proyecto Viaje Chile modifique archivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 $(function() {
 
-
     //globo tooltip
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
